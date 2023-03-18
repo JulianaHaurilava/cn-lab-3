0f474297d704efd15e964865044df0dffa2d15ee
Juliana Haurilava <jrediss.k@gmail.com>
2023-03-18 15:14:37 +0300

    Add project files.

---
+++ server/server/Component.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace server
+{
+    internal class Component
+    {
+        public string FactoryName { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime DeliveryDate { get; set; } 
+
+        public Component()
+        {
+            FactoryName = String.Empty;
+            Price = 0;
+            DeliveryDate = DateTime.MinValue;
+        }
+
+        [JsonConstructor]
+        public Component(string factoryName, double price, DateTime deliveryDate)
+        {
+            FactoryName = factoryName;
+            Price = price;
+            DeliveryDate = deliveryDate;
+        }
+
+        //public JObject GetJson()
+        //{
+        //    JObject jClient = new JObject();
+        //    jClient["FactoryName"] = FactoryName;
+        //    jClient["Price"] = Price;
+        //    jClient["DeliveryDate"] = DeliveryDate;
+        //    return jClient;
+        //}
+
+    }
+}
---
+++ server/server/Repository.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class Repository
+    {
+        protected readonly string fileName;
+        List<Component> componentList;
+
+        public Repository()
+        {
+            componentList = new List<Component>();
+        }
+
+        public List<Component> ReturnComponents(DateTime userDate)
+        {
+            List<Component> neededComponents = new();
+
+        }
+
+        public Repository()
+        {
+            fileName = "clients.json";
+            AllClients = new ObservableCollection<Client>();
+
+            OutOfFile();
+        }
+        private void OutOfFile()
+        {
+            if (File.Exists(fileName))
+            {
+                using (StreamReader stream = new StreamReader(fileName, true))
+                {
+                    string json = stream.ReadToEnd();
+                    if (json.Length != 0)
+                    {
+                        AllClients = JsonConvert.DeserializeObject<ObservableCollection<Client>>(json);
+                    }
+                }
+            }
+        }
+        private void InFile()
+        {
+            JArray clientsArray = new JArray();
+            foreach (Client client in AllClients)
+            {
+                clientsArray.Add(client.GetJson());
+            }
+
+            using (StreamWriter stream = new StreamWriter(fileName))
+            {
+                string json = JsonConvert.SerializeObject(clientsArray, Formatting.Indented);
+                stream.Write(json);
+            }
+        }
+    }
+}
